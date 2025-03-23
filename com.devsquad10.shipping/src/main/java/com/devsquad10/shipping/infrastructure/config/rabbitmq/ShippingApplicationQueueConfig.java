51c7c885d0b67ad86b6100aa12c59737f3a71fe7
minji <zz8168@naver.com>
2025-03-23 14:03:18 +0900

    fix: #122 메시지 dto 직렬화 config 설정 및 queue, exchange, binding 삭제

--- com.devsquad10.shipping/src/main/java/com/devsquad10/shipping/infrastructure/config/rabbitmq/ShippingApplicationQueueConfig.java
+++ com.devsquad10.shipping/src/main/java/com/devsquad10/shipping/infrastructure/config/rabbitmq/ShippingApplicationQueueConfig.java
@@ -12,93 +12,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ShippingApplicationQueueConfig {
 
-	// exchange
-	@Value("${shippingMessage.exchange.shipping.request}")
-	private String shippingCreateRequestExchange;
-
-	@Value("${shippingMessage.exchange.shipping.response}")
-	private String shippingCreateResponseExchange;
-
-	@Value("${shippingMessage.exchange.shipping_update.request}")
-	private String shippingUpdateRequestExchange;
-
-	@Value("${shippingMessage.exchange.shipping_update.response}")
-	private String shippingUpdateResponseExchange;
-
-	// queue
-	@Value("${shippingMessage.queue.shipping.request}")
-	private String queueShippingCreateRequest;
-
-	@Value("${shippingMessage.queue.shipping.response}")
-	private String queueShippingCreateResponse;
-
-	@Value("${shippingMessage.queue.shipping_update.request}")
-	private String queueShippingUpdateRequest;
-
-	@Value("${shippingMessage.queue.shipping_update.response}")
-	private String queueShippingUpdateResponse;
-
-	@Bean
-	public TopicExchange shippingCreateRequestExchange() {
-		return new TopicExchange(shippingCreateRequestExchange);
-	}
-
-	@Bean
-	public TopicExchange shippingCreateResponseExchange() {
-		return new TopicExchange(shippingCreateResponseExchange);
-	}
-
-	@Bean
-	public TopicExchange shippingUpdateRequestExchange() { return new TopicExchange(shippingUpdateRequestExchange); }
-
-	@Bean
-	public TopicExchange shippingUpdateResponseExchange() { return new TopicExchange(shippingUpdateResponseExchange); }
-
-	@Bean
-	public Queue queueShippingCreateRequest() {	return new Queue(queueShippingCreateRequest); }
-
-	@Bean
-	public Queue queueShippingCreateResponse() {
-		return new Queue(queueShippingCreateResponse);
-	}
-
-	@Bean
-	public Queue queueShippingUpdateRequest() { return  new Queue(queueShippingUpdateRequest); }
-
-	@Bean
-	public Queue queueShippingUpdateResponse() { return  new Queue(queueShippingUpdateResponse); }
-
-
-	@Bean
-	public Binding bindingRequestShipping() {
-		return BindingBuilder.bind(queueShippingCreateRequest())
-			.to(shippingCreateRequestExchange())
-			.with(queueShippingCreateRequest);
-	}
-
-	@Bean
-	public Binding bindingResponseShipping() {
-		return BindingBuilder.bind(queueShippingCreateResponse())
-			.to(shippingCreateResponseExchange())
-			.with(queueShippingCreateResponse);
-	}
-
-	@Bean
-	public Binding bindingUpdateRequestShipping() {
-		return BindingBuilder.bind(queueShippingUpdateRequest())
-			.to(shippingUpdateRequestExchange())
-			.with(queueShippingUpdateRequest);
-	}
-
-	@Bean
-	public Binding bindingUpdateResponseShipping() {
-		return BindingBuilder.bind(queueShippingUpdateResponse())
-			.to(shippingUpdateResponseExchange())
-			.with(queueShippingUpdateResponse);
-	}
-
 	@Bean
 	public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
 		return new Jackson2JsonMessageConverter();
 	}
+
 }
