411609750c249a1643188ea4f14150c873d6e5ef
minji <zz8168@naver.com>
2025-03-25 03:03:04 +0900

    fix: #194 슬랙 발송에 필요한 유저 feign client 오류로 인해 수정

--- com.devsquad10.shipping/src/main/java/com/devsquad10/shipping/infrastructure/client/dto/UserInfoFeignClientResponse.java
+++ com.devsquad10.shipping/src/main/java/com/devsquad10/shipping/infrastructure/client/dto/UserInfoFeignClientResponse.java
@@ -1,6 +1,6 @@
 package com.devsquad10.shipping.infrastructure.client.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,12 +8,10 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-@Builder
 @Data
-@AllArgsConstructor
 public class UserInfoFeignClientResponse {
+	@JsonProperty("username")
 	private String username;
+	@JsonProperty("slackId")
 	private String slackId;
 }
--- com.devsquad10.user/src/main/java/com/devsquad10/user/application/dto/UserInfoFeignClientResponse.java
+++ com.devsquad10.user/src/main/java/com/devsquad10/user/application/dto/UserInfoFeignClientResponse.java
@@ -1,14 +1,17 @@
 package com.devsquad10.user.application.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
+@Builder
+@Data
+@AllArgsConstructor
 public class UserInfoFeignClientResponse {
 	private String username;
 	private String slackId;
-
-	public UserInfoFeignClientResponse(String username, String slackId) {
-		this.username = username;
-		this.slackId = slackId;
-	}
 }
